7c93cdc9b7e2448aa53c99cfbbb9404a4334842e
Mantvydas Razulevicius <razuleviciusmantvydas@gmail.com>
2020-06-13 14:41:14 +0300

    Task 3 solved

--- SQL/2.sql
+++ SQL/2.sql
@@ -1,16 +1,32 @@
 SELECT 
   ISNULL(CAST(Users.ClientID AS char(3)), 'n/a') AS ClientID, 
   ISNULL(Sum(Orders.Total), 0.00) AS Total 
-FROM Users
-FULL OUTER JOIN Orders ON Users.UserID = Orders.UserID 
-GROUP BY (Users.ClientID)
-HAVING Sum(Orders.Total) >= 
-((SELECT top 1
-  Sum(Orders.Total)
-FROM Users
-FULL OUTER JOIN Orders ON Users.UserID = Orders.UserID 
-GROUP BY (Users.ClientID)
-HAVING Sum(Orders.Total) < 1100)
-);
-
-
+FROM 
+  Users FULL 
+  OUTER JOIN Orders ON Users.UserID = Orders.UserID 
+GROUP BY 
+  (Users.ClientID) 
+HAVING 
+  Sum(Orders.Total) >= (
+    (
+      SELECT 
+        top 1 Sum(Orders.Total) 
+      FROM 
+        Users FULL 
+        OUTER JOIN Orders ON Users.UserID = Orders.UserID 
+      GROUP BY 
+        (Users.ClientID) 
+      HAVING 
+        Sum(Orders.Total) < (
+          SELECT 
+            TOP 1 Sum(Orders.Total) 
+          FROM 
+            Users FULL 
+            OUTER JOIN Orders ON Users.UserID = Orders.UserID 
+          GROUP BY 
+            (Users.ClientID) 
+          ORDER BY 
+            Sum(Orders.Total) DESC
+        )
+    )
+  )
